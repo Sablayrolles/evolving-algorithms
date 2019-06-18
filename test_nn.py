b85ccb616dc79ae2984442319b2617c70d2f5ee4
Sablayrolles Louis <louis.sablayrolles@gmail.com>
2019-06-18 13:35:08 +0200

    create nn class for agent

--- test_nn.py
+++ test_nn.py
@@ -21,37 +21,61 @@ class NeuralNetwork:
         #type:
         """
         Conv1D(nb, kernel-size=1, activation=)
-        MaxPooling1D(pool-size=) # de combien on veut réduire si 800 au début et 400 a la fin alors =2
-        RNN(nb)
-        GRU(nb, activation=, recurrent-activation=)
-        LSTM(nb, activation=, recurrent-activation=)
+        MaxPooling1D(pool_size=) # de combien on veut réduire si 800 au début et 400 a la fin alors =2
+        GRU(nb, activation=, recurrent_activation=)
+        LSTM(nb, activation=, recurrent_activation=)
         """
-        if typeIn == "Dense":
-            model.add(Dense(int(nbIn), input_dim=nbIn, activation=activationIn))
+        if typeIn == "Dense" or typeIn == "Conv1D":
+            if typeIn == "Dense":
+                model.add(Dense(int(nbIn), input_dim=nbIn, activation=activationIn))
+            if typeIn == "Conv1D":
+                model.add(Conv1D(int(nbIn), input_shape=(1, int(nbIn)), kernel_size=1,  activation=activationIn))
+
+            #implement int
+            for nb, typ, act in zip(nbInt, typeInt, activationInt):
+                if typ == "Dense":
+                    model.add(Dense(int(nb), activation=act))
+                if typ == "Conv1D":
+                    model.add(Conv1D(int(nb), kernel_size=1, activation=act))
+                if typ == "MaxPooling1D":
+                    model.add(MaxPooling1D(int(nb), kernel_size=1, activation=act))
+                if typ == "GRU":
+                    model.add(GRU(int(nb), kernel_size=1, activation=act, recurrent_activation=act))
+                if typ == "LSTM":
+                    model.add(LSTM(int(nb), kernel_size=1, activation=act, recurrent_activation=act))
+                    
+            if typeOut == "Dense":
+                model.add(Dense(int(nbOut), activation=activationOut))
+            if typeOut == "Conv1D":
+                model.add(Conv1D(int(nbOut), kernel_size=1, activation=activationOut))
+            if typeOut == "MaxPooling1D":
+                model.add(MaxPooling1D(int(nbOut), kernel_size=1, activation=activationOut))
+            if typeOut == "GRU":
+                model.add(GRU(int(nbOut), kernel_size=1, activation=activationOut))
+            if typeOut == "LSTM":
+                model.add(LSTM(int(nbOut), kernel_size=1, activation=activationOut, recurrent_activation=activationOut))
             
-        #implement int
-        for nb, typ, act in zip(nbInt, typeInt, activationInt):
-            if typ == "Dense":
-                model.add(Dense(int(nb), activation=act))
+            model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
                 
-        if typeOut == "Dense":
-            model.add(Dense(int(nbOut), activation=activationOut))
+            self.model = model
         
-        model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-            
-        self.model = model
-        
-        #model.summary()
+            #model.summary()
+
+    def isSet(self):
+        return "model" in self.__dict__.keys()
         
     def setActions(self, actions):
         self.actions = actions
         
     def predict(self, env=[], ret="name"):
-        n = int(self.model.predict_classes(numpy.array([env])))
-        if ret == "name":
-            return self.actions[n]
-        else:
-            return n
+        if "model" in self.__dict__.keys():
+            n = int(self.model.predict_classes(numpy.array([env])))
+            if ret == "name":
+                return self.actions[n]
+            else:
+                return n
+        else:  
+            return False
 
 n = NeuralNetwork(3, "Dense", "relu", 4, "Dense", "sigmoid", [8,15,15,8], ["relu", "relu", "relu", "relu"], ["Dense", "Dense", "Dense", "Dense", "Dense"])
 n.setActions(["forward", "turnLeft", "turnRight", "backward"])
