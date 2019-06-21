3af73a4d5715544ac15bde21c12a107ed1d4d78e
Sablayrolles Louis <louis.sablayrolles@gmail.com>
2019-06-21 13:17:08 +0200

    Add generation multiple colors in map

--- test_pygame.py
+++ test_pygame.py
@@ -15,7 +15,7 @@ if not pygame.font:
 # up vers le haut et droite vers la droite dans une fenetre
 
 class Elipse:
-    def __init__(self, fminx, fmaxx, fminy, fmaxy, width, height, env, velocity=-1, color=(255,0,0), x = 0, y = 0):
+    def __init__(self, fminx, fmaxx, fminy, fmaxy, width, height, env, velocity=-1, color=(255,0,0), x = -1, y = -1):
         self.minx = fminx
         self.maxx = fmaxx
         self.miny = fminy
@@ -38,9 +38,9 @@ class Elipse:
             
         self.color = color
         
-        self.x = x
-        self.y = y
-        self.d = 1
+        self.x = x if x > -1 else random.randint(fminx, fmaxx)
+        self.y = y if y > -1 else random.randint(fminy, fmaxy)
+        self.d = random.randint(1,4)
         
     def getColor(self):
         return self.color
@@ -52,19 +52,21 @@ class Elipse:
         return (self.x + (self.w / 2), self.y + (self.h / 2))
     
     def getHead(self):
-        if self.d == 1:
-            return (self.x + (self.w / 2), self.y)
         if self.d == 2:
-            return (self.x + (self.w / 2), self.y + self.h)
+            return (self.x + (self.w / 2), self.y)
         if self.d == 3:
-            return (self.x, self.y + (self.h / 2))
+            return (self.x + (self.w / 2), self.y + self.h)
         if self.d == 4:
+            return (self.x, self.y + (self.h / 2))
+        if self.d == 1:
             return (self.x + self.w, self.y + (self.h / 2))
     
     def randomMove(self):
         #print("Pos:",(self.x, self.y),"->",self.env.getCase(self.x, self.y))
         #print("dir:", ["up", "right", "down", "left"][self.d-1])
-        #print("perception:", self.env.getPerception(self.x, self.y, 3, self.d))
+        #print("range:", self.w)
+        print("color: ", self.getColor())
+        print("perception:", self.env.getPerception(self.x, self.y, self.w, self.d))
         
         self.d = random.randint(1,4)
         self.forward()
@@ -130,24 +132,66 @@ class Elipse:
         pygame.draw.line(win, (255,255,255), self.getCenter(), self.getHead())
 
 class EnvWorld:
-    #!! coords inverted -> end dir tout est ok
     def __init__(self, fminx, fmaxx, fminy, fmaxy):
-        self.map = [[random.randint(1,5) for _ in range(fmaxy - fminy)] for _ in range(fmaxx - fminx)]
+        self.map = [[(random.choice([2, 3, 4, 5, 6]) if random.randint(1, 1000) == 1 else 0) for _ in range(fmaxy - fminy)] for _ in range(fmaxx - fminx)]
         #remplir que avec des valeurs positives
-
-        self.map[5][5] = "x"
-        self.printMap()
 
         self.minx = fminx
         self.miny = fminy
         self.maxx = fmaxx
         self.maxy = fmaxy
 
+        #self.expendValues()
+        #self.expendValues()
+
+        print("Init map")
+        self.printMap()
+
+    def getDims(self):
+        return (len(self.map), len(self.map[0]))
+
+    def expendValues(self):
+        map2 = self.map.copy()
+        for x in range(self.maxx - self.minx):
+            for y in range(self.maxy - self.miny):
+                if self.map[x][y] == 2:
+                    neighbors = self.getCasesNeighbors(x, y)
+
+                    for (x2, y2) in neighbors:
+                        if self.isValidCoords(x2, y2):
+                            try:
+                                map2[x2][y2] = 2
+                            except IndexError:
+                                print("Index : ",x2, y2)
+        self.map = map2.copy()
+
     def printMap(self):
         print("[")
         for l in self.map:
             print(l)
         print("]")
+
+    def draw(self, win):
+        for x in range(self.maxx - self.minx):
+            for y in range(self.maxy - self.miny):
+                if self.map[x][y] == 2:
+                    pygame.draw.ellipse(win, (0, 255, 0), (self.minx+x, self.miny+y, 3, 3))
+                if self.map[x][y] == 3:
+                    pygame.draw.ellipse(win, (255, 0, 0), (self.minx+x, self.miny+y, 3, 3))
+                if self.map[x][y] == 4:
+                    pygame.draw.ellipse(win, (255, 140, 0), (self.minx+x, self.miny+y, 3, 3))
+                if self.map[x][y] == 5:
+                    pygame.draw.ellipse(win, (70, 130, 180), (self.minx+x, self.miny+y, 3, 3))
+                if self.map[x][y] == 6:
+                    pygame.draw.ellipse(win, (138, 43, 226), (self.minx+x, self.miny+y, 3, 3))
+
+    def getCasesNeighbors(self, posx, posy):
+        bloc = self.getFacesCases(posx, posy, 1, 1)
+        bloc.extend(self.getFacesCases(posx, posy, 2, 1))
+        bloc.extend(self.getFacesCases(posx, posy, 3, 1))
+        bloc.extend(self.getFacesCases(posx, posy, 4, 1))
+
+        return list(set(bloc))
 
     def getFacesCases(self, posx, posy, direction, nbSide):
         if direction == 1: #up
@@ -171,7 +215,7 @@ class EnvWorld:
         bloc.extend(bloc_devant)
         bloc.extend(bloc_a_droite)
 
-        return bloc
+        return list(bloc)
 
     def isValidCoords(self, posx, posy):
         return posx >= self.minx and posx <= self.maxx and posy >= self.miny and posy <= self.maxy
@@ -183,15 +227,15 @@ class EnvWorld:
 
         percep = []
         for case in facesCases:
-            if self.isValidCoords(case[1], case[0]):
-                percep.append(self.map[case[1]][case[0]])
+            if self.isValidCoords(case[0], case[1]):
+                percep.append(self.map[case[0]][case[1]])
             else:
                 percep.append(-1)
 
         return percep
 
     def getCase(self, posx, posy):
-        return self.map[posy][posx]
+        return self.map[posx][posy]
         
 class Tick:
     def __init__(self, win):
@@ -201,13 +245,13 @@ class Tick:
         
     def tick(self):
         s = "Tick :"+str(self.t)
-        label = self.myfont.render(s, 1, (0,0,0))
+        label = self.myfont.render(s, 1, (0, 0, 0))
         self.win.blit(label, (0, 0))
         
         self.t += 1
         s = "Tick :"+str(self.t)
         
-        label = self.myfont.render(s, 1, (0,255,0))
+        label = self.myfont.render(s, 1, (255, 255, 255))
         self.win.blit(label, (0, 0))
 
 class Window:
@@ -224,14 +268,17 @@ class Window:
 def myrun(win, width, height):
     run = True
     env = EnvWorld(0, width, 0, height)
-    ellipses = [Elipse(0, width, 0, height, 1, 1, env), 
-                ]#Elipse(0, width, 0, height, 10, 15, env, color=(255,0,255)), 
-                #Elipse(0, width, 0, height, 10, 15, env, color=(0,255,0))]
+    ellipses = [Elipse(0, width, 0, height, 10, 10, env), 
+                Elipse(0, width, 0, height, 10, 10, env, color=(255, 0, 255)), 
+                Elipse(0, width, 0, height, 10, 10, env, color=(0, 255, 0))]
     
     t = Tick(win)
+    env.draw(win)
+    pygame.display.update()
 
+    print("Run")
     while run:
-        pygame.time.delay(500)
+        pygame.time.delay(50)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -245,11 +292,13 @@ def myrun(win, width, height):
             e.undraw(win)
             e.randomMove()
             e.draw(win)
+
+        env.draw(win)
         
         t.tick()
         
         pygame.display.update()
     pygame.quit()
 
-myWin = Window(10, 10, "Test bestioles")
+myWin = Window(640, 480, "Test bestioles")
 myWin.run(myrun)
